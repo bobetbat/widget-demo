be89c4fcf74654c06f967d36cfcf2d3d146df9e7
bobetbat <b.bobetayev.97@gmail.com>
2024-07-08 00:47:54 +0500

    chore: clean up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,10 @@
 import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
-import { config } from '../wagmi';
-
-const client = new QueryClient();
+import EstimateGasWidget from 'estimate-gas-widget';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <EstimateGasWidget />
   );
 }
 
